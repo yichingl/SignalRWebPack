047d5dff706364b8c4846a452bdb4d67057be87b
Annabelle <yiching.a.lee@gmail.com>
2019-11-22 11:02:41 -0500

    updated node version; changed typo in scheme, causing UI to have different origin from the one specified in server's Startup.cs

--- SignalRWebPack/src/js/index.ts
+++ SignalRWebPack/src/js/index.ts
@@ -1,14 +1,37 @@
 ﻿import "../css/main.css";
 import { global_obj } from "./testGlobal";
 import { testRsHub } from "./Hubs/test_RsMainHub";
-import { vikHub } from "./Hubs/RsMainHub";
-import { libraryHub } from "./Hubs/RsMainHub";
+//import { vikHub } from "./Hubs/RsMainHub";
+//import { libraryHub } from "./Hubs/RsMainHub";
 
+import * as signalR from '@aspnet/signalr';
 
 const divMessages: HTMLDivElement = document.querySelector("#divMessages");
 const tbMessage: HTMLInputElement = document.querySelector("#tbMessage");
 const btnSend: HTMLButtonElement = document.querySelector("#btnSend");
 const username = new Date().getTime();
+
+
+// TEST CODE //
+let SYSTEM_ROOT = "http://localhost:5003";
+let vikConnection = new signalR.HubConnectionBuilder().withUrl(`${SYSTEM_ROOT}/vik`).build();
+
+let RS_vikConnected = false;
+
+vikConnection.on("SendVIKConnectionStatus", function (status) {
+    console.log("vik connection: " + status);
+
+});
+
+vikConnection.on("SendVIKData", function (dataArray) {
+    console.log("Got " + dataArray.length + " VIK values");
+});
+
+vikConnection.start().then(function () {
+    console.log("connected  to vik hub");
+
+});
+// END TEST CODE //
 
 
 
@@ -44,6 +67,6 @@ console.log("exporting to library...");
 export {
     testRsHub,
     global_obj,
-    vikHub,
-    libraryHub
+    //vikHub,
+    //libraryHub
 }
