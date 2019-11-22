e0333790e6a30ac3ec73a922911898794addc366
Annabelle <yiching.a.lee@gmail.com>
2019-11-22 09:43:43 -0500

    name change ASL server url var; changed app url to 1121

--- SignalRWebPack/src/js/Hubs/RsMainHub.ts
+++ SignalRWebPack/src/js/Hubs/RsMainHub.ts
@@ -1,10 +1,10 @@
 ﻿import { RsVikHub } from "./RsVikHub";
 import { RsLibraryHub } from "./RsLibraryHub";
 
-let SYSTEM_ROOT = "http://localhost:5003";
+let SERVER_ROOT = "http://localhost:5003";
 
-export var vikHub = new RsVikHub(SYSTEM_ROOT);
-export var libraryHub = new RsLibraryHub(SYSTEM_ROOT);
+export var vikHub = new RsVikHub(SERVER_ROOT);
+export var libraryHub = new RsLibraryHub(SERVER_ROOT);
 
 console.info("Vik Hub Connected: " + vikHub.connected);
 console.info("Library Hub Connected: " + libraryHub.connected);
