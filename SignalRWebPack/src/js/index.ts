3a7e1dddceb9293a714e896a00fae2d26f20ccae
Annabelle <yiching.a.lee@gmail.com>
2019-11-22 16:15:54 -0500

    removed extra example files

--- SignalRWebPack/src/js/index.ts
+++ SignalRWebPack/src/js/index.ts
@@ -1,44 +1,9 @@
 ﻿import "../css/main.css";
-import { testRsHub } from "./Hubs/test_RsMainHub";
 import { vikHub } from "./Hubs/RsMainHub";
 import { libraryHub } from "./Hubs/RsMainHub";
 
-const divMessages: HTMLDivElement = document.querySelector("#divMessages");
-const tbMessage: HTMLInputElement = document.querySelector("#tbMessage");
-const btnSend: HTMLButtonElement = document.querySelector("#btnSend");
-const username = new Date().getTime();
-
-/*
-
-testRsHub.getConnection().on("messageReceived", (username: string, message: string) => {
-    let messageContainer = document.createElement("div");
-
-    messageContainer.innerHTML =
-        `<div class="message-author">${username}</div><div>${message}</div>`;
-
-    divMessages.appendChild(messageContainer);
-    divMessages.scrollTop = divMessages.scrollHeight;
-});
-
-testRsHub.getConnection().start().catch(err => document.write(err));
-
-tbMessage.addEventListener("keyup", (e: KeyboardEvent) => {
-    if (e.keyCode === 13) {
-        console.log("sending message...")
-        send();
-    }
-});
-
-btnSend.addEventListener("click", send);
-
-function send() {
-    testRsHub.getConnection().send("newMessage", username, tbMessage.value)
-        .then(() => tbMessage.value = "");
-}
-*/
-
+console.log(2);
 export {
-    testRsHub,
     vikHub,
     libraryHub
 }
--- SignalRWebPack/src/js/testGlobalHelper.ts
+++
@@ -1,4 +0,0 @@
-﻿import { global_obj } from "./testGlobal";
-console.log("Global num2: " + global_obj.printValue());
-global_obj.updateValue(22);
-console.log("Global num2: " + global_obj.printValue());
