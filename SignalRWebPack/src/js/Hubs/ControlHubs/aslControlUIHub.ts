6a71438a0fb60c33c08881640c07136297066167
Annabelle <yiching.a.lee@gmail.com>
2019-11-27 11:17:15 -0500

    added status change function

--- SignalRWebPack/src/js/Hubs/ControlHubs/aslControlUIHub.ts
+++ SignalRWebPack/src/js/Hubs/ControlHubs/aslControlUIHub.ts
@@ -6,6 +6,7 @@ export class ASLControlUIHub extends BaseHub {
         this.initConnection();
     }
 
+    // INIT METHODS
     protected configureEvents() {
         var thisHub = this;
         this.addEvent("broadcastASLStatusChange", (aslState: string) => {
@@ -14,10 +15,54 @@ export class ASLControlUIHub extends BaseHub {
 
         console.log(`HUB: "${this.getHubPath()}" has been configured.`);
     }
+    
 
     // EVENT HANDLERS
     private gotASLStatusChange(aslState: string) {
         console.log(`HUB: "${this.getHubPath()}" has state - ${aslState}`);
 
     }
+
+    // UI HELPER FUNCTIONS
+    private setStatusText(status) {
+        var statusText = status;
+        switch (status) {
+            case "CONNECTED":
+                statusText = "Connected";
+                //$("#simController").switchClass("fa-pause-circle", "fa-play-circle");
+                //$(".spin-on-status").removeClass("fa-spin");
+                break;
+            case "UNCONNECTED":
+                statusText = "Not Connected";
+                //$("#simController").switchClass("fa-pause-circle", "fa-play-circle");
+                //$(".spin-on-status").removeClass("fa-spin");
+                break;
+            case "SIMULATING":
+                statusText = "Running Simulation";
+                //$("#simController").switchClass("fa-play-circle", "fa-pause-circle");
+                //$(".spin-on-status").removeClass("fa-spin");
+                //$(".spin-on-status").addClass("fa-spin");
+                break;
+            case "SIMULATIONSTARTED":
+                statusText = "Simulation Started";
+                //$("#simController").switchClass("fa-play-circle", "fa-pause-circle");
+                //$(".spin-on-status").removeClass("fa-spin");
+                //$(".spin-on-status").addClass("fa-spin");
+                break;
+            case "INITIALIZESIMULATION":
+                statusText = "Initializing...";
+                //$("#simController").switchClass("fa-pause-circle", "fa-play-circle");
+                //$(".spin-on-status").removeClass("fa-spin");
+                break;
+            case "STOPSIMULATION":
+                statusText = "Stopping...";
+                //$("#simController").switchClass("fa-pause-circle", "fa-play-circle");
+                //$(".spin-on-status").removeClass("fa-spin");
+                break;
+        }
+        //$('.system-status').text(statusText);
+    }
+
+
+    // NON UI HELPER FUNCTIONS
 }
