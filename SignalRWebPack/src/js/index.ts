84dbc007f5877fe5cd551c9e1e85cba5db209413
Annabelle <yiching.a.lee@gmail.com>
2019-12-18 09:55:29 -0500

    added jquery dependency and typing

--- SignalRWebPack/src/js/index.ts
+++ SignalRWebPack/src/js/index.ts
@@ -1,5 +1,11 @@
 ﻿import "../css/main.css";
 import { hubs } from "./Hubs/initHubs";
+import * as $ from 'jquery';
+
+$(document).ready(function () {
+    console.log("hello");
+});
+
 
 export {
     hubs
